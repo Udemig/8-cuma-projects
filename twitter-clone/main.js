47a7b7e06eddc11558ffb0cafd07ac5c0bf58ec7
furkanevin <furkanevin00@gmail.com>
2023-12-30 14:56:52 +0300

    twitter finish

--- twitter-clone/main.js
+++ twitter-clone/main.js
@@ -1,14 +1,81 @@
+import API from './scripts/api.js';
 import { getLocal } from './scripts/helpers.js';
 import {
   ele,
   renderUserInfo,
   renderTimeline,
   renderLoader,
+  renderDetailLoader,
+  renderDetail,
+  renderUser,
 } from './scripts/ui.js';
-import API from './scripts/api.js';
 
 // lokal'den kullanıcı bilgilerini al
 const user = getLocal('user');
+
+// kullanıcnı hangi sayfayı göreceğine karar veren fonksiyon
+// ekranın orta kısmına yer alıcak html kodunu belirler
+const router = () => {
+  // url'deki arama paramtrelerine erişme
+  const params = new URLSearchParams(location.search);
+  const page = params.get('page');
+  const query = params.get('q');
+
+  //  page'İn değerine göre aryüze karar ver
+  switch (page) {
+    // tweet detay
+    case 'status':
+      // lodaingi ekrana bas
+      renderDetailLoader('Gönderi');
+      // tweet detaylarını api'dan
+      API.getData(`/tweet.php?id=${query}`)
+        // erkana detay sayfasını bas
+        .then((data) => renderDetail(data, user));
+
+      break;
+
+    // arama sayfası
+    case 'search':
+      renderDetailLoader(`${query} için sonuçlar`);
+
+      API.getData(`/search.php?query=${query}&search_type=top`)
+        //
+        .then((data) => renderTimeline(data, ele.main, 'user_info'));
+
+      break;
+
+    // kullnaıcı detay sayfası
+    case 'user':
+      // sayfanın yüklendiğini belirt
+      renderDetailLoader(query);
+
+      // kulalncın bilgilerini api'dan al
+      API.getUser(query).then((user) => {
+        // kullanıcn hesap bilgilerini ekrana bas
+        renderUser(user);
+        // tweet'lerin geliceği yeri seçme
+        const outlet = document.querySelector('.page-bottom');
+        // kullanıcn attığı tweetleri al
+        API.getData(`/timeline.php?screenname=${query}`).then(
+          (data) => renderTimeline(data, outlet, 'author')
+        );
+      });
+
+      break;
+
+    // varasayılan olaraka anasayfayı ekrana bas
+    default:
+      //1) ekrana yükleniyor bas
+      renderLoader(ele.tweetsArea);
+
+      // 2) Kullanıcın tweet'lerini al
+      API.getData(`/timeline.php?screenname=${user.profile}`)
+        // 3) tweet'leri ekrana bas
+        .then((data) =>
+          renderTimeline(data, ele.tweetsArea, 'author')
+        );
+  }
+};
 
 // sayfa yüklenince kullanıcının bilgilerini ekrana bas
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,13 +87,8 @@ document.addEventListener('DOMContentLoaded', () => {
     location = '/auth.html';
   }
 
-  //1) ekrana yükleniyor bas
-  renderLoader(ele.tweetsArea);
-
-  // 2) Kullanıcın tweet'lerini al
-  API.getData(`/timeline.php?screenname=${user.profile}`)
-    // 3) tweet'leri ekrana bas
-    .then((data) => renderTimeline(data, ele.tweetsArea));
+  // ekran basılcak sayfayı belirle
+  router();
 });
 
 // çıkış butonuna tıklanınca oturumu kapat
@@ -38,4 +100,22 @@ ele.logoutBtn.addEventListener('click', () => {
 
   // logine yönlendit
   location = '/auth.html';
+});
+
+// geri butona tıklanma olayını izle
+ele.main.addEventListener('click', (e) => {
+  if (e.target.id === 'back-btn') {
+    // bir adım geriye git
+    history.back();
+  }
+});
+
+// arama formunun gönderilmesini izle
+ele.searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // formdaki veriye eriş
+  const query = e.target[0].value;
+
+  // sayfayı değiş
+  location = `?page=search&q=${query}`;
 });
--- twitter-clone/scripts/ui.js
+++ twitter-clone/scripts/ui.js
@@ -5,6 +5,7 @@ export const ele = {
   tweetsArea: document.querySelector('.tweets-area'),
   logoutBtn: document.querySelector('#logout-btn'),
   main: document.querySelector('main'),
+  searchForm: document.querySelector('aside form'),
 };
 
 // kullanıcın bilgilerini ekrana basar
@@ -19,6 +20,9 @@ export const renderUserInfo = (user) => {
 
 // tweetin medya içeriğini alıcak ve içerğe göre html oluşturucak
 const getMedia = (media) => {
+  // media içeriği yoksa
+  if (!media) return '';
+
   // fotoğraf varsa
   if (media.photo) {
     return media.photo
@@ -31,15 +35,12 @@ const getMedia = (media) => {
     const url = media.video[0].variants[0].url;
     return `<video controls src="${url}"/>`;
   }
-
-  // media içeriği yoksa
-  return '';
 };
 
 // tweetleri ekana basar
 // 1) data:tweet'ler
 // 2) outlet: hangi elementin içerisne göndericez
-export const renderTimeline = (data, outlet) => {
+export const renderTimeline = (data, outlet, user) => {
   console.log(data.timeline[1]);
 
   // her bir tweet için outlet'e bir tweet divi bas
@@ -48,19 +49,17 @@ export const renderTimeline = (data, outlet) => {
       (tweet) => `
         <div class="tweet">
           <img id="user-img" src="${
-            tweet.author
-              ? tweet.author.avatar
-              : '/images/defaultt.png'
+            tweet[user] ? tweet[user].avatar : '/images/defaultt.png'
           }" />
           <div class="body">
             <div class="user">
             ${
-              tweet.author
+              tweet[user]
                 ? `
-               <a href="#">
-                <img id="mobile-img" src="${tweet.author?.avatar}" />
-                <b>${tweet.author?.name}</b>
-                <p>@${tweet.author?.screen_name}</p>
+               <a href="?page=user&q=${tweet[user].screen_name}">
+                <img id="mobile-img" src="${tweet[user]?.avatar}" />
+                <b>${tweet[user]?.name}</b>
+                <p>@${tweet[user]?.screen_name}</p>
                 <p>${moment(tweet.created_at).fromNow()}</p>
               </a>
               `
@@ -71,10 +70,12 @@ export const renderTimeline = (data, outlet) => {
               <i class="bi bi-three-dots"></i>
             </div>
 
-            <div class="content">
+            <a href="?page=status&q=${
+              tweet.tweet_id
+            }" class="content">
               <p>${tweet.text}</p>
               ${getMedia(tweet.media)}
-            </div>
+            </a>
 
             <div class="buttons">
               <button>
@@ -105,4 +106,213 @@ export const renderTimeline = (data, outlet) => {
 export const renderLoader = (outlet) => {
   outlet.innerHTML =
     '<div id="loader-wrapper"> <div aria-live="assertive" role="alert" class="loader"></div> </div>';
+};
+
+// detay sayfasının yükleniyorunu ekrana basar
+export const renderDetailLoader = (text) => {
+  ele.main.innerHTML = `
+  <div class="nav">
+    <i id="back-btn" class="bi bi-arrow-left"></i>
+    <h4>${text}</h4>
+  </div>
+
+
+  <div id="loader-wrapper"> <div aria-live="assertive" role="alert" class="loader"></div> </div>
+  `;
+};
+
+// ekrana tweet detay sayfasını basar
+export const renderDetail = (tweet, user) => {
+  ele.main.innerHTML = `
+      <div class="nav">
+        <i id="back-btn" class="bi bi-arrow-left"></i>
+        <h4>Gönderi</h4>
+      </div>
+
+      <div class="tweet detail-tweet">
+        <img
+          id="user-img"
+          src="${tweet.author.image}"
+        />
+
+        <div class="body">
+          <div  class="user">
+            <a href="?page=user&q=${tweet.author.screen_name}">
+              <img
+                id="mobile-img"
+                src="${tweet.author.image}"
+              />
+              <b>${tweet.author.name}</b>
+              <p>@${tweet.author.screen_name}</p>
+            </a>
+            <button>Takip Et</button>
+          </div>
+
+          <div class="content">
+            <p>${tweet.text}</p>
+            ${getMedia(tweet.media)}
+          </div>
+
+          <div class="info">
+            <p>${tweet.created_at}</p>
+            <p><span>${
+              tweet.views
+            }</span><span>Görüntülenme</span></p>
+          </div>
+
+          <div class="buttons">
+            <button>
+              <i class="bi bi-chat"></i>
+              <span>${tweet.replies}</span>
+            </button>
+            <button>
+              <i class="bi bi-recycle"></i>
+              <span>${tweet.retweets}</span>
+            </button>
+            <button>
+              <i class="bi bi-heart"></i>
+              <span>${tweet.likes}</span>
+            </button>
+            <button>
+              <i class="bi bi-bookmark"></i>
+              <span>${tweet.bookmarks}</span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <form id="comment-form">
+        <img
+          src="${user.avatar}"
+        />
+        <input placeholder="Yanıtını Gönder" type="text" />
+        <button>Yanıtla</button>
+      </form>
+  `;
+};
+
+// kullanıcı syafaını ekrana basar
+export const renderUser = (user) => {
+  console.log(user);
+  ele.main.innerHTML = `
+      <div class="user-page">
+        <!-- üst kısım -->
+        <div class="page-top">
+          <!-- nav -->
+          <div id="nav">
+            <i id="back-btn" class="bi bi-arrow-left"></i>
+            <div>
+              <h3>${user.name}</h3>
+              <p>
+                <span>${Math.round(Math.random() * 3000)}</span>
+                <span>Gönderi</span>
+              </p>
+            </div>
+          </div>
+
+          <!-- banner -->
+          <div class="banner">
+            <img src="${user.header_image}" />
+            <img
+              id="user-pp"
+              src="${user.avatar}"
+            />
+          </div>
+
+          <!-- butonlar -->
+          <div class="buttons">
+            <div class="icon">
+              <i class="bi bi-three-dots"></i>
+            </div>
+            <div class="icon">
+              <i class="bi bi-envelope"></i>
+            </div>
+            <button>Takip Et</button>
+          </div>
+
+          <!-- kullanıcı bilgileri -->
+          <div class="info">
+            <h4>
+              <span>${user.name}</span>
+
+              ${
+                user.blue_verified &&
+                '<i class="bi bi-patch-check-fill"></i>'
+              }
+              
+            </h4>
+
+            <p class="profile">@${user.profile}</p>
+
+            <p class="description">
+              ${user.desc}
+            </p>
+
+            <div>
+              <a href="#">
+                <span>${user.friends}</span>
+                <span>Takip Edilen</span>
+              </a>
+              <a href="#">
+                <span>${user.sub_count}</span>
+                <span>Takipçi</span>
+              </a>
+            </div>
+          </div>
+
+          <!-- butonlar -->
+          <div class="radio-input">
+            <label>
+              <input
+                value="value-1"
+                name="value-radio"
+                id="value-1"
+                type="radio"
+                checked=""
+              />
+              <span>Gönderiler</span>
+            </label>
+            <label>
+              <input
+                value="value-2"
+                name="value-radio"
+                id="value-2"
+                type="radio"
+              />
+              <span>Yanıtlar</span>
+            </label>
+            <label>
+              <input
+                value="value-3"
+                name="value-radio"
+                id="value-3"
+                type="radio"
+              />
+              <span>Medya</span>
+            </label>
+            <label>
+              <input
+                value="value-3"
+                name="value-radio"
+                id="value-3"
+                type="radio"
+              />
+              <span>Beğeni</span>
+            </label>
+            <span class="selection"></span>
+          </div>
+        </div>
+
+        <!-- alt kısım -->
+        <div class="page-bottom">
+          <div id="loader-wrapper">
+            <div
+              aria-live="assertive"
+              role="alert"
+              class="loader"
+            ></div>
+          </div>
+        </div>
+      </div>  
+  `;
 };
